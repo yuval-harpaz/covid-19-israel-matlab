81174b56b31eebd6a2d88ac5c94bd22c460bfe11
yuval <yuval@laptop1910>
2020-05-14 22:54:21 +0300

    daily update

---
+++ code/covid_israel_measures.m
@@ -0,0 +1,11 @@
+list = readtable('~/covid-19_data_analysis/data/Israel/Israel_ministry_of_health.csv');
+idx = ~isnan(list.hospitalized);
+figure;
+plot(find(idx),list.hospitalized(idx))
+x = datetime('14-Mar-2020','22-Mar-2020',)
+label = {'schools and restaurants','work places','
+    
+plane = imread('https://p7.hiclipart.com/preview/190/547/594/airplane-computer-icons-symbol-cargo-plane-shipping-transportation-wings-icon.jpg');
+plane = plane(164:346,106:398,1) > 100;
+axes('pos',[.1 .6 .5 .3])
+imshow('coins.png')
