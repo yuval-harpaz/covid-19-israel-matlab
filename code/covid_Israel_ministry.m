45428904dcb4b0fa03ba709064fde58da72ff3be
yuval <yuval@laptop1910>
2020-06-18 20:35:00 +0300

    daily update

--- code/covid_Israel_ministry.m
+++ code/covid_Israel_ministry.m
@@ -11,19 +11,11 @@ iHref = iHref(find(iHref < iLink,1,'last'));
 link = ['https://govextra.gov.il/',txt(iHref+7:iLink+(length(ext)-1))];
 [~,~] = system(['wget ',link]);
 movefile(link(find(ismember(link,'/'),1,'last')+1:end),['data/Israel/covid19-data-israel',ext])
-if strcmp(ext,'.csv') % remove commas
-    if exist('data/Israel/tmp.csv','file')
-        !rm data/Israel/tmp.csv
-    end
-    !awk -F'"' -v OFS='' '{ for (i=2; i<=NF; i+=2) gsub(",", "", $i) } 1' data/Israel/covid19-data-israel.csv >> data/Israel/tmp.csv
-    movefile('data/Israel/tmp.csv','data/Israel/covid19-data-israel.csv')
+if exist('data/Israel/tmp.csv','file')
+    !rm data/Israel/tmp.csv
 end
-iLink = strfind(txt,'.xlsx');
-iHref = strfind(txt,'href');
-iHref = iHref(find(iHref < iLink,1,'last'));
-link = ['https://govextra.gov.il/',txt(iHref+7:iLink+4)];
-[~,~] = system(['wget ',link]);
-movefile(link(find(ismember(link,'/'),1,'last')+1:end),'data/Israel/covid19-data-israel.xlsx')
+!awk -F'"' -v OFS='' '{ for (i=2; i<=NF; i+=2) gsub(",", "", $i) } 1' data/Israel/covid19-data-israel.csv >> data/Israel/tmp.csv
+movefile('data/Israel/tmp.csv','data/Israel/covid19-data-israel.csv')
 
 %%
 % ! google-chrome https://datadashboard.health.gov.il/COVID-19/?utm_source=go.gov.il&utm_medium=referral
