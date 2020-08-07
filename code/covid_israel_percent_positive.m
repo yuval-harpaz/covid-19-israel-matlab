bfc05f8819ba659d8e3293cd628bd01f71b6fc6e
yuval <yuval@laptop1910>
2020-08-07 23:40:31 +0300

    loop

--- code/covid_israel_percent_positive.m
+++ code/covid_israel_percent_positive.m
@@ -44,15 +44,33 @@ end
 % hold on
 % scatter(datetime('18-Apr-2020'),2.7,'fill','k')
 % legend('בדיקות','בדיקות חיוביות','18-Apr')
+hospSmooth = movmean(list.new_hospitalized(idx),[3 3]);
+hospSmooth(end) = nan;
+hospSmooth(end-1) = mean(list.new_hospitalized(idx(end-4:end-1)));
+hospSmooth(end-2) = mean(list.new_hospitalized(idx(end-5:end-1)));
+hospSmooth(end-3) = mean(list.new_hospitalized(idx(end-6:end-1)));
+pos = round(100*list.tests_positive(idx)./list.tests_result(idx),1);
+pos1 = pos;
+pos1(80:85) = linspace(pos(80),pos(85),6);
+posSmooth = movmean(pos1,[3 3]);
+posSmooth(end) = nan;
+posSmooth(end-1) = mean(pos(end-4:end-1));
+posSmooth(end-2) = mean(pos(end-5:end-1));
+posSmooth(end-3) = mean(pos(end-6:end-1));
+
 fig1 = figure('position',[50,50,800,500]); %#ok<NASGU>
 yyaxis left
 plot(list.date(idx),list.new_hospitalized(idx));
+hold on
+plot(list.date(idx),hospSmooth,'linewidth',2,'linestyle','-');
 ylabel('new hospitalized')
 ax = gca;
 ax.YRuler.Exponent = 0;
 
 yyaxis right
-plot(list.date(idx),round(100*list.tests_positive(idx)./list.tests_result(idx),1))
+plot(list.date(idx),pos)
+hold on
+plot(list.date(idx),posSmooth,'linewidth',2,'linestyle','-');
 ylabel('positive tests (%)')
 set(gca,'ygrid', 'on','fontsize',13)
 xlim([list.date(idx(1))-1 list.date(idx(end))+1]);
