62c8ee1f241ea547f286d77f9ba404378aba46a0
yuval <yuval@lenovo.2020>
2020-11-02 23:52:11 +0200

    daily update

--- code/predict_pos60.py
+++ code/predict_pos60.py
@@ -14,6 +14,22 @@ s = requests.get(url).content
 tests = pd.read_csv(io.StringIO(s.decode('utf-8')))
 #  Moving average smoothiong
 N = 7
-pos60 = np.convolve(tests.pos_m_60+tests.pos_f_60, np.ones((N,))/N, mode='valid')
+fm = tests.pos_f_60+tests.pos_m_60
+pos60 = np.convolve(fm, np.ones((N,))/N, mode='valid')
+pos60 = np.concatenate(([0,0,0],pos60))
+for i_end in [6,5,4]:
+    pos60 = np.concatenate((pos60,[np.mean(fm[-i_end:])]))
+
 dead = np.convolve(dashboard.CountDeath, np.ones((N,))/N, mode='valid')
-numpy.convolve(a, v, mode='full')[source]
+dead[0:19] = 0
+dead = np.concatenate(([0,0,0],dead))
+for i_end in [6,5,4]:
+    dead = np.concatenate((dead,[np.mean(dashboard.CountDeath[-i_end:])]))
+
+# predict
+pred = np.convolve(pos60, probability['all'], mode='full')
+
+fig, axs = plt.subplots()
+plt.plot(pred/10)
+plt.plot(dead)
+plt.show()
