df88cce3f2aceaa15a5f7a4e881f2a09c5668d06
yuval <yuval@lenovo.2020>
2020-11-20 00:10:41 +0200

    daily update

--- code/covid_stringency.m
+++ code/covid_stringency.m
@@ -1,31 +1,73 @@
-[~,~] = system('wget -O tmp.json https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/date-range/2020-02-15/2020-11-11');
-fid = fopen('tmp.json','r');
-json = fread(fid);
-fclose(fid)
-json = jsondecode(native2unicode(json)');
-dates = fieldnames(json.data);
-date = datetime(strrep(strrep(dates,'x',''),'_','-'));
-for ii = 1:length(date)
-    dat = eval(['json.data.',dates{ii},'.ISR']);
-    if isempty(dat.deaths)
-        deaths(ii,1) = nan;
-    else
-        deaths(ii,1) = dat.deaths;
-    end
-    if isempty(dat.confirmed)
-        confirmed(ii,1) = nan;
-    else
-        confirmed(ii,1) = dat.confirmed;
-    end
-    stringency(ii,1) = dat.stringency;
+function covid_stringency(country,download)
+% covid_stringency({'SVN','SI'});
+% covid_stringency({'CZE','CZ'});
+if ~exist('country')
+    country = [];
 end
+if isempty(country)
+    country = {'Israel','IL'};
+end
+if ~exist('download','var')
+    download = false;
+end
+cd ~/covid-19-israel-matlab/data
+% d = dir('tmp.json');
+% if length(d) == 1
+%     if dateshift(datetime(d.date),'start','day') == datetime('today')
+%         download = false;
+%     end
+% end
+if download       
+%     [~,~] = system('wget -O tmp.json https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/date-range/2020-02-15/2020-11-11');
+    [~,~] = system('wget -O tmp1.csv https://raw.githubusercontent.com/OxCGRT/covid-policy-tracker/master/data/OxCGRT_latest.csv');
+end
+% fid = fopen('tmp.json','r');
+% json = fread(fid);
+% fclose(fid)
+% json = jsondecode(native2unicode(json)');
+ox = readtable('tmp1.csv');
+dates = str(ox.Date(contains(ox.CountryName,country{1})));
+date = datetime(dates,'InputFormat','yyyyMMdd');
+% deaths = ox.ConfirmedDeaths(ismember(ox.CountryName,country{1}));
 
-t = table(date,stringency,confirmed,deaths);
-t.deaths(ismember(t.date,datetime(2020,8,20))) = nan;
+[dataMatrix] = readCoronaData('deaths');
+[~,timeVector,mergedData] = processCoronaData(dataMatrix);
+[isx,idx] = ismember(date,timeVector);
+deaths = zeros(length(date),1);
+deaths(isx) = mergedData{contains(mergedData(:,1),country{1}),2}(idx(isx))';
+deaths(deaths == 0) = nan;  % end of vector?
+% deaths = deaths(ismember(timeVector,date));
+stringency = ox.StringencyIndex(contains(ox.CountryName,country{1}));
+% dates = fieldnames(json.data);
+% date = datetime(strrep(strrep(dates,'x',''),'_','-'));
+% for ii = 1:length(date)
+%     if isfield(eval(['json.data.',dates{ii}]),codes{1})
+%         dat = eval(['json.data.',dates{ii},'.',codes{1}]);
+%         if isempty(dat.deaths)
+%             deaths(ii,1) = nan;
+%         else
+%             deaths(ii,1) = dat.deaths;
+%         end
+%         if isempty(dat.confirmed)
+%             confirmed(ii,1) = nan;
+%         else
+%             confirmed(ii,1) = dat.confirmed;
+%         end
+%         stringency(ii,1) = dat.stringency;
+%     else
+%         deaths(ii,1) = deaths(ii-1,1);
+%         confirmed(ii,1) = confirmed(ii-1,1);
+%         stringency(ii,1) = stringency(ii-1,1);
+%     end
+% end
 
-
-mob = readtable('~/Downloads/Region_Mobility_Report_CSVs/2020_IL_Region_Mobility_Report.csv');
-mob = mob(1:find(contains(mob.sub_region_1,'Center District'),1)-1,8:end);
+t = table(date,stringency,deaths);
+if ismember('IL',country)
+    t.deaths(ismember(t.date,datetime(2020,8,20))) = nan;
+end
+t = t(1:find(~isnan(t.stringency),1,'last')-1,:);
+mob = readtable(['~/Downloads/Region_Mobility_Report_CSVs/2020_',country{2},'_Region_Mobility_Report.csv']);
+mob = mob(1:find(~cellfun(@isempty,mob.sub_region_1),1)-1,8:end);
 [isx,idx] = ismember(t.date,mob.date);
 figure;
 fill([t.date;flipud(t.date)],[t.stringency;-flipud(t.stringency)],[0.9,0.9,0.9],'linestyle','none')
@@ -33,8 +75,8 @@ hold on
 colorset;
 plot(mob.date,movmedian(mob{:,2:end},[3 3]))
 plot(t.date(2:end),diff(t.deaths),'k')
-legend('מדד צעדי מנע של אוקספורד','חנויות','מכולות','פארקים','תחנות אוטובוס','עבודה','בית','תמותה')
-title('השוואה בין המדדים של אוקספורד לגוגל לחומרת צעדי המנע בישראל')
+legend('מדד צעדי המנע של אוקספורד','חנויות','מכולות','פארקים','תחנות אוטובוס','עבודה','בית','תמותה')
+title({'השוואה בין המדדים של אוקספורד לגוגל לחומרת צעדי המנע',country{1}})
 grid on
 box off
 set(gcf,'Color','w')
