0acb8c7cbdd9a82e8c3e1cc8a20b2248d503d51d
yuval <yuval@lenovo.2020>
2020-11-25 11:49:04 +0200

    daily update

--- code/covid_nyc.m
+++ code/covid_nyc.m
@@ -1,15 +1,16 @@
 function [date,pop,deceased,deceased_probable] = covid_nyc
 % nyc = urlread('https://raw.githubusercontent.com/nychealth/coronavirus-data/master/Deaths/probable-confirmed-dod.csv');
-nyc = urlread('https://raw.githubusercontent.com/nychealth/coronavirus-data/master/deaths/probable-confirmed-dod.csv');
-nyc = strrep(nyc,'/20,','/2020,');
+% nyc = urlread('https://raw.githubusercontent.com/nychealth/coronavirus-data/master/deaths/probable-confirmed-dod.csv');
+nyc = urlread('https://raw.githubusercontent.com/nychealth/coronavirus-data/master/trends/data-by-day.csv');
+% nyc = strrep(nyc,'/20,','/2020,');
 fid = fopen('tmp.csv','w');
 fwrite(fid,nyc);
 fclose(fid);
 nyc = readtable('tmp.csv');
 !rm tmp.csv
-nyc.PROBABLE_COUNT(isnan(nyc.PROBABLE_COUNT)) = 0;
-nyc.CONFIRMED_COUNT(isnan(nyc.CONFIRMED_COUNT)) = 0;
-date = nyc.DATE_OF_DEATH;
+% nyc.PROBABLE_COUNT(isnan(nyc.PROBABLE_COUNT)) = 0;
+% nyc.CONFIRMED_COUNT(isnan(nyc.CONFIRMED_COUNT)) = 0;
+date = nyc.date_of_interest;
 pop = 8399000;
 deceased = cumsum(nyc.CONFIRMED_COUNT);
 deceased_probable = deceased + cumsum(nyc.PROBABLE_COUNT);
