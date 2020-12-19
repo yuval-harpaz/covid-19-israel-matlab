3275810b22d95eeb81198d80f8fa329d20d5beb2
yuval <yuval@lenovo.2020>
2020-12-19 20:27:38 +0200

    daily update

--- code/covid_brazil.m
+++ code/covid_brazil.m
@@ -4,7 +4,8 @@ if nargin == 0
 end
 % bra = urlread('https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-cities-time.csv');
 disp('downloading Brazil')
-[~,~] = system('wget -O tmp.csv https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-cities-time.csv')
+% [~,~] = system('wget -O tmp.csv https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-cities-time.csv')
+[~,~] = system('wget -O tmp.csv https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-states.csv');
 % fid = fopen('tmp.csv','w');
 % fwrite(fid,bra);
 % fclose(fid);
@@ -37,13 +38,78 @@ for ii = 1:length(state)
         end
     end
 end
-
+%%
 if plt
-    [~,order] = sort(yy(end,:),'descend');
     ypm = yy./pop.pop'*10^6;
+    [~,order] = sort(ypm(end,:),'descend');
     figure;
     h = plot(date,ypm);
     for ii = 1:height(pop)
-        text(date(end),ypm(end,ii),pop.state_name{ii},'Color',h(ii).Color);
+        text(date(end)+5,ypm(end,ii),pop.state_name{ii},'Color',h(ii).Color);
     end
+    dypm = diff(ypm);
+    bad = dypm(2:end,:)-dypm(1:end-1,:) > 20;
+    dypm(bad) = nan;
+    dypm = movmean(movmedian(dypm,[3 3],'omitnan'),[3 3]);
+    legend(h(order),pop.state_name(order))
+    set(gca,'FontSize',13)
+    set(gcf,'Color','w')
+    xlim([datetime(2020,3,15) datetime('today')+1])
+    title('תמותה למליון מצטברת בברזיל, לפי איזור')
+    box off
+    grid on
+    yt = linspace(min(dypm(end,:)),max(dypm(end,:)),size(dypm,2));
+    figure;
+    hd = plot(date(2:end),dypm);
+    hold on
+    for ii = 1:height(pop)
+        if ~ismember(ii,[3,6,14])
+            hd(ii).Color = [0 0 0];
+        end
+        text(date(end)+5,yt(ii),pop.state_name{ii},'Color',hd(ii).Color);
+    end
+    for ii = [3,6,14]
+        text(date(end)+5,yt(ii),pop.state_name{ii},'Color',hd(ii).Color);
+        plot(date(2:end),dypm(:,ii),'Color',hd(ii).Color);
+    end
+    legend(hd([3,6,14]),pop.state_name([3,6,14]))
+    set(gca,'FontSize',13)
+    set(gcf,'Color','w')
+    xlim([datetime(2020,3,15) datetime('today')+1])
+    title('תמותה למליון ליום בברזיל, לפי איזור')
+    box off
+    grid on
+end
+%%
+% https://github.com/wcota/covid19br/raw/master/cases-brazil-cities-time.csv.gz
+city = false
+if city
+    !wget -O tmp.gz https://github.com/wcota/covid19br/raw/master/cases-brazil-cities-time.csv.gz
+    gunzip('tmp.gz')
+    movefile('tmp','tmp.csv')
+    t = readtable('tmp.csv');
+    t(ismember(t.state,'TOTAL'),:) = [];
+    date = unique(t.date);
+    cityName = unique(t.city);
+    iMa = find(contains(cityName,'Manaus'));
+    figure;
+    for ii = 1:length(cityName)
+        iCity = ismember(t.city,cityName{ii});
+        dpm = diff(t.deaths_per_100k_inhabitants(iCity)*10);
+        dpm = [0;movmean(movmedian(dpm,[3 3]),[3 3])];
+        if ii == 1
+            h1 = plot(t.date(iCity),dpm,'k');
+            hold on
+        elseif ii == iMa
+            h2 = plot(t.date(iCity),dpm/10,'r');
+        else
+            plot(t.date(iCity),dpm,'k');
+        end
+    end
+    iCity = ismember(t.city,cityName{iMa});
+    dpm = diff(t.deaths_per_100k_inhabitants(iCity)*10);
+    dpm = [0;movmean(movmedian(dpm,[3 3]),[3 3])];
+    plot(t.date(iCity),dpm,'r','linewidth',2);
+    legend([h2,h1(1)],'מנאוס','ערים אחרות')
+    title('תמותה בערי ברזיל')
 end
