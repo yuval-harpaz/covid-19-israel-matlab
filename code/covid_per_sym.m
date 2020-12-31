fdfc8385d771977672fc4d5ea1596283bd2b6e55
yuval <yuval@lenovo.2020>
2020-12-31 21:04:41 +0200

    daily update

--- code/covid_per_sym.m
+++ code/covid_per_sym.m
@@ -1,4 +1,5 @@
 tests = readtable('tests.csv');
+perSym = 100*(tests.symptoms_pos_f_60+tests.symptoms_pos_m_60)./(tests.nosymptoms_pos_m+tests.nosymptoms_pos_f);
 perSmSm = perSym;
 perSmSm(1:end-6) = movmean(perSmSm(1:end-6),[3 3],'omitnan');
 figure;plot(tests.date,perSmSm)
