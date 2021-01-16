c83f306306a294cb9bc8fc39aff94d98133ca53f
yuval <yuval@lenovo.2020>
2021-01-16 18:00:14 +0200

    daily update

--- code/covid_age_plot.m
+++ code/covid_age_plot.m
@@ -127,7 +127,7 @@ bad = sum(yDash,2) == 0;
 
 figure;
 % subplot(1,2,1)
-plot(staffDate(~bad),yDash(~bad,1:2))
+plot(staffDate(~bad),yDash(~bad,2:-1:1))
 hold on
 colorset
 plot(tsevet.Date,tsevet{:,2:3},'--')
@@ -142,15 +142,17 @@ xtickformat('MMM')
 xlim([datetime(2020,6,15) datetime('today')+5])
 title('צוות רפואי מאומת לפי לוח הבקרה ומאגר המידע')
 
-
-figure;plot(staffDate(~bad),yDash(~bad,:))
-legend('רופאים מאומתים','אחיות מאומתות','רופאים מבודדים','אחיות מבודדות')
+tit = {'רופאות מאומתות','אחים מאומתים','רופאות מבודדות','אחים מבודדים'};
+figure;
+hIso = plot(staffDate(~bad),yDash(~bad,:),'marker','.');
+legend(hIso([3,4,1,2]),tit([4,3,2,1]),'location','north')
 box off
 grid on
 set(gcf,'Color','w')
 xtickformat('MMM')
 xlim([datetime(2020,6,15) datetime('today')])
-title('צוות רפואי מאומת לפי לוח הבקרה')
+title(['צוות רפואי לפי לוח הבקרה עד ',datestr(staffDate(end),'dd/mm hh:MM')])
+xlim([datetime(2020,10,1) datetime('today')+5])
 
 %% 
 tests = readtable('tests.csv');
@@ -165,3 +167,27 @@ set(gcf,'Color','w')
 title('מאומתים סימפטומטיים')
 legend('מבוגרים מ 60','צעירים מ 60')
 
+%%
+badDoc = bad;
+badDoc(190) = true;
+nurse = yDash(:,1);
+nurse(80:87) = nan;
+listD = readtable('dashboard_timeseries.csv');
+figure;
+% subplot(1,2,1)
+fill(datetime(2020,12,[20,20+14,20+14,20,20]),[0,0,2,2,0]/0.3,[0.9,0.9,0.9],'linestyle','none')
+hold on
+plot(staffDate(~badDoc),yDash(~badDoc,2)/200/0.3)
+hold on
+plot(staffDate(~bad),nurse(~bad)/400/0.3)
+plot(listD.date,movmean(listD.tests_positive,[3 3])/4000/0.3)
+xtickformat('MMM')
+xlim([datetime(2020,6,15) datetime('today')+5])
+title('מאומתים לפי לוח הבקרה')
+box off
+grid on
+legend('שבועיים מחיסון ראשון','רופאים','אחים','הציבור הכללי','location','northwest')
+set(gcf,'Color','w')
+xtickformat('MMM')
+ylabel('מנורמל לפי תחילת דצמבר')
+xlabel('גרף מאומתים בישראל לכבוד בארד 2134')
--- code/covid_vent_by_hosp.m
+++ code/covid_vent_by_hosp.m
@@ -1,8 +1,38 @@
-t = readtable('~/Downloads/vent_per_hosp - Sheet1.csv');
-t = t(ismember(t.date,datetime(2021,1,1)),:)
-figure; h = bar([t.vent,t.crit-t.vent],'stacked'); legend(h([2,1]),'קריטיים לא מונשמים','קריטיים מונשמים')
+cd ~/covid-19-israel-matlab/data/Israel
+t = readtable('vent_per_hosp.csv');
+t1 = t(ismember(t.date,datetime(2021,1,1)),:)
+figure;
+h = bar([t1.vent,t1.crit-t1.vent],'stacked'); legend(h([2,1]),'קריטיים לא מונשמים','קריטיים מונשמים')
 set(gcf,'Color','w')
-set(gca,'XTickLabel',t.hosp,'xtick',1:height(t),'ygrid','on')
+set(gca,'XTickLabel',t1.hosp,'xtick',1:height(t1),'ygrid','on')
 xtickangle(90)
 box off
-ylim([0 40])
+ylim([0 50])
+grid on
+
+t1 = t(ismember(t.date,datetime(2021,1,14)),:)
+figure;
+h = bar([t1.vent,t1.crit-t1.vent],'stacked'); legend(h([2,1]),'קריטיים לא מונשמים','קריטיים מונשמים')
+set(gcf,'Color','w')
+set(gca,'XTickLabel',t1.hosp,'xtick',1:height(t1),'ygrid','on')
+xtickangle(90)
+box off
+ylim([0 50])
+grid on
+
+figure;
+h3 = bar([t1.vent-t1.ecmo,t1.ecmo,t1.crit-t1.vent],'stacked','EdgeColor','none');
+tmp = h3(2).FaceColor;
+h3(2).FaceColor = h3(3).FaceColor;
+h3(3).FaceColor = tmp;
+legend(h3([3,2,1]),'ללא הנשמה \ אקמו','אקמו','הנשמה חודרנית')
+set(gcf,'Color','w')
+set(gca,'XTickLabel',t1.hosp,'xtick',1:height(t1),'ygrid','on')
+xtickangle(90)
+box off
+ylim([0 50])
+grid on
+title('הטיפול בחולים הקריטיים לפי בית חולים')
+
+
+t2 = t(ismember(t.date,datetime(2021,1,12)),:)
