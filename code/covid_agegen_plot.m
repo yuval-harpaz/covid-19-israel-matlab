7358f0322841dbf01dfb1c8ca85be17c61db54c4
yuval <yuval@lenovo.2020>
2021-01-19 09:05:38 +0200

    daily update

---
+++ code/covid_agegen_plot.m
@@ -0,0 +1,35 @@
+function covid_agegen_plot
+
+cd ~/covid-19-israel-matlab/data/Israel
+agegen = readtable('dashboard_age_gen.csv');
+col = [0.259 0.525 0.961;0.063 0.616 0.345;0.961 0.706 0.4;0.988 0.431 0.016;0.863 0.267 0.216];
+tit = {'מאומתים','קשים','מונשמים','נפטרים'};
+for iSec = 1:4
+    prevCol = iSec*20-20;
+    
+    y = [sum(agegen{:,prevCol+(2:5)},2),sum(agegen{:,prevCol+(6:9)},2),sum(agegen{:,prevCol+(10:13)},2),sum(agegen{:,prevCol+(14:17)},2),sum(agegen{:,prevCol+(18:21)},2)]
+    yd = diff(y);
+    yds = movmean(movmedian(yd,[3 3]),[3 3]);
+    ydsp = yds./sum(yds,2)*100;
+    sm = sum(y,2);
+    sm = sm/max(sm)*100;
+    
+    figure;
+    h = plot(agegen.date(2:end),ydsp);
+    for ih = 1:5
+        h(ih).Color = col(ih,:);
+    end
+    grid on
+    xtickformat('MMM')
+    
+    figure;
+    hb = bar(agegen.date(2:end),ydsp,20,'stacked','EdgeColor','none');
+    for ih = 1:5
+        hb(ih).FaceColor = col(ih,:);
+    end
+    grid on
+    xtickformat('MMM')
+    ylim([0 100])
+    hold on
+    plot(agegen.date(2:end),sm(2:end),'k')
+end
