27b530c5aab56f3e381bb8793b97f08c7d468665
yuval <yuval@lenovo.2020>
2021-02-12 14:17:47 +0200

    daily update

---
+++ code/covid_feb11.m
@@ -0,0 +1,64 @@
+time = {'Dose I +13';'Dose I 14+';'Dose II +6';'Dose II 7-14';'Dose II 14+'};
+community60 = [10724;;991;976];
+community = [16620;5198;966;639];
+mild60 = [375;224;38;46];
+mild = [130;33;nan;nan];
+moderate60 = [218;115;16;nan];
+moderate = [60;nan;0;nan];
+severe60 = [232;164;46;36];
+severe = [71;20;nan;nan];
+critical60 = [43;37;11;nan];
+critical = [11;nan;nan;nan];
+deceased60 = [311;165;32;17];
+deceased = [16;nan;nan;0];
+total60 = [7190;5017;1134;1093];
+total = [16908;5273;977;643];
+t = table(time,community60,mild60,moderate60,severe60,critical60,deceased60,total60,...
+    community,mild,moderate,severe,critical,deceased,total);
+t = readtable('~/Downloads/11 Feb 2021 - Sheet1.csv');
+y = t{:,3:7};
+y = y./sum(sum(y,2))*100;
+% r = t{4,8}-nansum(t{4,2:7});
+% y(4,[2,4]) = r*[2/3,1/3];
+% y = y./t{:,7}*100;
+figure;
+subplot(1,2,1)
+h = bar(y,'stacked','EdgeColor','none');
+for ii = 1:5
+    col(ii,1:3) = h(ii).FaceColor;
+end
+h(1).FaceColor = col(5,:);
+h(2).FaceColor = col(1,:);
+h(3).FaceColor = col(4,:);
+h(4).FaceColor = col(2,:);
+h(5).FaceColor = [0,0,0];
+legend(fliplr(h),fliplr({'Mild','Moderate','Severe','Critical','Deceased'}))
+set(gca,'XTickLabel',{'dose I, days 0-13';'dose I, 14 days or more';'dose II, days 0-6';'dose II, days 7-14';'dose II, more than 14 days'},'ygrid','on')
+text([1,1,1,1,1]-0.15,cumsum(y(1,:))-1,num2str(t{1,3:7}'),'Color','w')
+text([2,2,2,2,2]-0.15,cumsum(y(2,:))-1,num2str(t{2,3:7}'),'Color','w')
+ylabel('hospitalized (% of all 60+ vaccinated patients)')
+box off
+title('Older than 60')
+ylim([0 80])
+set(gca,'FontSize',13)
+xtickangle(15)
+
+y = t{:,9:13};
+y = y./sum(sum(y,2))*100;
+subplot(1,2,2)
+h = bar(y,'stacked','EdgeColor','none');
+h(1).FaceColor = col(5,:);
+h(2).FaceColor = col(1,:);
+h(3).FaceColor = col(4,:);
+h(4).FaceColor = col(2,:);
+h(5).FaceColor = [0,0,0];
+% legend(fliplr(h),fliplr({'Mild','Moderate','Severe','Critical','Deceased'}))
+set(gca,'XTickLabel',{'dose I, days 0-13';'dose I, 14 days or more';'dose II, days 0-6';'dose II, days 7-14';'dose II, more than 14 days'},'ygrid','on')
+text([1,1,1,1,1]-0.15,cumsum(y(1,:))-1,num2str(t{1,9:13}'),'Color','w')
+text([2,2,2,2,2]-0.15,cumsum(y(2,:))-1,num2str(t{2,9:13}'),'Color','w')
+ylabel('hospitalized (% of all <60 vaccinated patients)')
+box off
+title('Younger than 60')
+set(gcf,'Color','w')
+set(gca,'FontSize',13)
+xtickangle(15)
---
+++ code/covid_old_survuval.m
@@ -0,0 +1,74 @@
+cd ~/covid-19-israel-matlab/data/Israel
+listName = {'','severe_','ventilated_','deaths_'};
+tit = {'מאומתים','קשים','מונשמים','נפטרים'};
+col = [0.259 0.525 0.961;0.063 0.616 0.345;0.961 0.706 0.4;0.988 0.431 0.016;0.863 0.267 0.216];
+for iList = [1,4]
+    txt = urlread(['https://raw.githubusercontent.com/dancarmoz/israel_moh_covid_dashboard_data/master/',listName{iList},'ages_dists.csv']);
+    txt = txt(find(ismember(txt,newline),1)+1:end);
+    fid = fopen('tmp.csv','w');
+    fwrite(fid,txt);
+    fclose(fid);
+    ag = readtable('tmp.csv');
+    agDate = cellfun(@(x) datetime([x(1:10),' ',x(12:19)]),ag.UpdateTime);
+    casesYOday = [sum(ag{:,[2:7,12:17]},2),sum(ag{:,[8:11,18:21]},2)];
+    %%
+    yyy0 = movmean(diff(casesYOday)./datenum(diff(agDate)),[11 11],'omitnan');
+    yyy0 = movmean(yyy0,[11 11],'omitnan');
+    
+    clear yy;
+    for ii = 1:5
+        idx = 1+(ii*2-1:ii*2);
+        yy(1:length(agDate),ii) = sum(ag{:,idx},2);
+        disp(idx)
+    end
+%     if iList == 1 || iList == 4
+%         yyy = movmean(diff(yy)./datenum(diff(agDate)),[11 11],'omitnan');
+%     else
+    tmpY = [];
+    for iDate = 1:length(yy)
+        jDate = find(ismember(agDate,agDate(iDate)),1,'last');
+        if jDate == iDate
+            tmpY(iDate,1:5) = yy(jDate,:);
+        else
+            tmpY(iDate,1:5) = nan;
+        end
+    end
+    yyy = diff(movmean(tmpY(2:end,:),[11 11],'omitnan'));
+%     end
+    yyy = [cumsum(yyy,2);zeros(size(yyy))];
+    xxx = [agDate(3:end);flipud(agDate(3:end))];
+    
+    figure;
+    subplot(2,1,1)
+    for ii = 1:5
+        fill(xxx,yyy(:,6-ii),col(6-ii,:),'linestyle','none')
+        hold on
+    end
+    % plot(agDate(2:end),yyy)
+    legend('80+','60-80','40-60','20-40','0-20','location','northwest');
+    set(gcf,'Color','w')
+    ax = gca;
+    ax.YRuler.Exponent = 0;
+    xtickformat('MMM')
+    xlim(agDate([2,end]))
+    grid on
+    title([tit{iList},' ','לפי גיל'])
+    set(gca, 'layer', 'top');
+    
+    yyy1 = yyy./yyy(:,5)*100;
+    yyy1(isnan(yyy1)) = 0;
+    subplot(2,1,2)
+    for ii = 1:5
+        fill(xxx,yyy1(:,6-ii),col(6-ii,:),'linestyle','none')
+        hold on
+    end
+    % legend('80+','60-80','40-60','20-40','0-20','location','northwest');
+    set(gcf,'Color','w')
+    ax = gca;
+    ax.YRuler.Exponent = 0;
+    xtickformat('MMM')
+    xlim(agDate([2,end]))
+    grid on
+      title([tit{iList},' ','לפי גיל','%'])
+    set(gca, 'layer', 'top');
+end
