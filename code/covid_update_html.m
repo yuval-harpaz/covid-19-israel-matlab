f1e415c407af8088743b40eb1bf222412f6b0bcf
yuval <yuval@lenovo.2020>
2021-03-23 23:16:43 +0200

    daily update

--- code/covid_update_html.m
+++ code/covid_update_html.m
@@ -3,30 +3,30 @@ function covid_update_html
 cd ~/covid-19-israel-matlab/
 
 list = readtable('data/Israel/dashboard_timeseries.csv');
-yesterdate = datestr(datetime-1,'dd.mm.yyyy');
-today = datestr(datetime,'dd.mm.yyyy');
+% yesterdate = datestr(datetime-1,'dd.mm.yyyy');
+% today = datestr(datetime,'dd.mm.yyyy');
 % highest countries
-fName = 'docs/highest_countries.html';
-fid = fopen(fName,'r');
-txt = fread(fid);
-fclose(fid);
-txt = native2unicode(txt');
-iDate = strfind(txt,'2021');
-txt(iDate-6:iDate+3) = yesterdate;
-fid = fopen(fName,'w');
-fwrite(fid,txt);
-fclose(fid);
-% realigned
-fName = 'docs/realigned.html';
-fid = fopen(fName,'r');
-txt = fread(fid);
-fclose(fid);
-txt = native2unicode(txt');
-iDate = strfind(txt,'2021');
-txt(iDate-6:iDate+3) = yesterdate;
-fid = fopen(fName,'w');
-fwrite(fid,txt);
-fclose(fid);
+% fName = 'docs/highest_countries.html';
+% fid = fopen(fName,'r');
+% txt = fread(fid);
+% fclose(fid);
+% txt = native2unicode(txt');
+% iDate = strfind(txt,'2021');
+% txt(iDate-6:iDate+3) = yesterdate;
+% fid = fopen(fName,'w');
+% fwrite(fid,txt);
+% fclose(fid);
+% % realigned
+% fName = 'docs/realigned.html';
+% fid = fopen(fName,'r');
+% txt = fread(fid);
+% fclose(fid);
+% txt = native2unicode(txt');
+% iDate = strfind(txt,'2021');
+% txt(iDate-6:iDate+3) = yesterdate;
+% fid = fopen(fName,'w');
+% fwrite(fid,txt);
+% fclose(fid);
 % index
 fName = 'docs/README.md';
 fid = fopen(fName,'r');
@@ -34,7 +34,7 @@ txt = fread(fid);
 fclose(fid);
 txt = native2unicode(txt');
 iDate = strfind(txt,'2021');
-txt(iDate-6:iDate+3) = yesterdate;
+txt(iDate-6:iDate+3) = datestr(list.date(end),'dd.mm.yyyy');
 fid = fopen(fName,'w');
 fwrite(fid,txt);
 fclose(fid);
@@ -45,49 +45,33 @@ txt = fread(fid);
 fclose(fid);
 txt = native2unicode(txt');
 iDate = strfind(txt,'2021');
-txt(iDate-6:iDate+3) = today;
-% counter1 = strfind(txt,'יום ממוות אחד');
-% counter0 = strfind(txt,'כיום,');
-% count = datenum(datetime('today'))-datenum(datetime('22-Jan-2020'))-65;
-% txt = [txt(1:counter0-1),'כיום, ',str(count),' ',txt(counter1:end)];
+txt(iDate-6:iDate+3) = datestr(list.date(end),'dd.mm.yyyy');
 idx = strfind(txt,'מצב התמותה');
-% daten = datenum(list.date);
-% daten = daten-daten(1);
-% last5 = find(daten > daten(end)-4,1);
  list.CountDeath(isnan(list.CountDeath)) = 0;
 y = cumsum(list.CountDeath);
 y = y(end-7:end-1);
 x = (1:7)';
 x = [ones(length(x),1) x];
-% y = list.deceased(last5:end);
 b = x\y;
-
 ins = ['מצב התמותה - ', '(בשבעת הימים האחרונים)',' כ ',str(round(b(2))),' נפטרים ליום ',];
 txt = [txt(1:idx-1),ins,txt(idx+find(ismember(txt(idx:end),'<'),1)-1:end)];
-
 idx = strfind(txt,'מצב המונשמים');
-
-
 list.CountBreath(isnan(list.CountBreath)) = 0;
 y = list.CountBreath(end-7:end-1);
-% y = list.CountBreath(last5:end);
 b = x\y;
 mun7 = round(b(2));
-
 if mun7 >= 0
     ins = ['מצב המונשמים - ', '(בשבעת הימים האחרונים)',' כל יום יש כ ',str(mun7),' מונשמים יותר  ',];
 else
     ins = ['מצב המונשמים - ', '(בשבעת הימים האחרונים)',' כל יום יש כ ',str(-mun7),' מונשמים פחות',];
 end
 txt = [txt(1:idx-1),ins,txt(idx+find(ismember(txt(idx:end),'<'),1)-1:end)];
-
 fid = fopen(fName,'w');
 fwrite(fid,unicode2native(txt));
 fclose(fid);
 %% push
-
 [~,~] = system('git add -A');
 [~,~] = system('git commit -m "daily update"');
 [~,~] = system('git pull');
 [~,~] = system('git push');
-%% old
+
