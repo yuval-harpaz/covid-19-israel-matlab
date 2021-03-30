64fcdb4057531f3a29a010294b12b34f942b0f94
yuval <yuval@lenovo.2020>
2021-03-30 10:02:30 +0300

    daily update

---
+++ code/covid_R.m
@@ -0,0 +1,27 @@
+json = urlread('https://datadashboardapi.health.gov.il/api/queries/infectionFactor');
+json = jsondecode(json);
+t = struct2table(json);
+date = datetime(strrep(t.day_date,'T00:00:00.000Z',''));
+ful = ~cellfun(@isempty ,t.R);
+R = nan(length(date),1);
+R(ful,1) = cellfun(@(x) x,t.R(ful));
+
+
+listD = readtable('~/covid-19-israel-matlab/data/Israel/dashboard_timeseries.csv');
+mm = movmean(listD.tests_positive,[6 0]);
+rr = mm(15:end)./mm(1:end-14);
+
+figure;
+yyaxis left
+plot(date,R,':')
+hold on;
+plot(listD.date(1:end-14),rr.^0.3)
+ylim([0 2])
+ylabel('R')
+yyaxis right
+plot(listD.date,listD.tests_positive)
+xtickformat('MMM')
+set(gca,'xtick',datetime(2020,4:30,1))
+set(gcf,'Color','w')
+title('cases vs R מאומתים מול')
+ylabel('Cases')
