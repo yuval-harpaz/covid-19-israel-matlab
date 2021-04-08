0c9bf0866a4587e4147dfd891c8af123dcb290d0
yuval <yuval@lenovo.2020>
2021-04-08 11:05:30 +0300

    daily update

--- code/covid_R.m
+++ code/covid_R.m
@@ -10,18 +10,24 @@ R(ful,1) = cellfun(@(x) x,t.R(ful));
 listD = readtable('~/covid-19-israel-matlab/data/Israel/dashboard_timeseries.csv');
 mm = movmean(listD.tests_positive,[6 0]);
 rr = mm(15:end)./mm(1:end-14);
-
+%%
 figure;
 yyaxis left
-plot(date,R,':')
-hold on;
-plot(listD.date(1:end-14),rr.^0.3)
+plot(date,R)
+% hold on;
+% plot(listD.date(1:end-14),rr.^0.3,':','LineWidth',1.5)
 ylim([0 2])
 ylabel('R')
 yyaxis right
-plot(listD.date,listD.tests_positive)
+plot(listD.date,movmean(listD.tests_positive,[3 3]))
+hold on
+plot(listD.date,listD.tests_positive,':')
+ylim([0 10000])
 xtickformat('MMM')
 set(gca,'xtick',datetime(2020,4:30,1))
 set(gcf,'Color','w')
 title('cases vs R מאומתים מול')
 ylabel('Cases')
+xlim([datetime(2020,6,1) datetime('tomorrow')])
+legend('R','cases','location','north')
+grid on
---
+++ code/covid_crit2.m
@@ -0,0 +1,27 @@
+cd ~/covid-19-israel-matlab/data/Israel
+listD = readtable('dashboard_timeseries.csv');
+newc = readtable('new_critical.csv');
+ward = readtable('crit_by_ward.xlsx');
+col = [0,0.447,0.741;0.850,0.325,0.0980;0.929,0.694,0.125;0.494,0.184,0.556;0.466,0.674,0.188;0.301,0.745,0.933;0.635,0.0780,0.184];
+death = movmean(listD.CountDeath,[3 3]);
+idx = 188:height(listD);
+%%
+figure;
+hh(1) = scatter(listD.date(idx),listD.CountBreath(idx),'.','MarkerEdgeColor',col(1,:),'MarkerEdgeAlpha',0.5);
+hold on
+hh(2) = plot(listD.date(idx),movmean(listD.CountBreath(idx),[3 3]),'-','Color',col(1,:),'linewidth',1.5);
+hh(3) = scatter(listD.date(idx),listD.CountCriticalStatus(idx),'.','MarkerEdgeColor',col(2,:),'MarkerEdgeAlpha',0.5);
+hh(4) = plot(listD.date(idx),movmean(listD.CountCriticalStatus(idx),[3 3]),'-','Color',col(2,:),'linewidth',1.5);
+hh(5) = plot(ward.date,ward.corona,'--','Color',col(2,:),'linewidth',1.5);
+hh(6) = plot(listD.date,death,'k')
+grid on
+% ylim([0 350])
+legend(hh([4,5,2,6]),'all critical        סך הכל קריטיים','corona ward     מחלקת קורונה',...
+    'all on vent      סך הכל מונשמים','deaths                         תמותה','location','northwest')
+xlim([datetime(2020,8,15) datetime('today')])
+set(gcf,'Color','w')
+title({'חולים במצב קריטי','critical patients'})
+ylabel('patients  חולים')
+set(gca,'xtick',datetime(2020,4:30,1))
+xtickformat('MMM')
+ylim([0 450])
