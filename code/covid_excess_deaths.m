e8a6371a4f0ce1f6546d042ea0343a06bc435ec7
yuval <yuval@lenovo.2020>
2021-05-28 16:32:31 +0300

    daily update

--- code/covid_excess_deaths.m
+++ code/covid_excess_deaths.m
@@ -3,30 +3,30 @@ cd ~/covid-19-israel-matlab/data/Israel
 % [~,msg] = system('wget -O lmsMonth.xlsx https://www.cbs.gov.il/he/publications/DocLib/2020/%D7%9C%D7%95%D7%97%D7%95%D7%AA%20%D7%AA%D7%9C%D7%95%D7%A9%D7%99%D7%9D/%D7%A4%D7%98%D7%99%D7%A8%D7%95%D7%AA-%D7%A9%D7%A0%D7%94-%D7%97%D7%95%D7%93%D7%A9.xlsx');
 [~,msg] = system('wget -O lmsMonth.xlsx https://www.cbs.gov.il/he/publications/LochutTlushim/2020/%D7%A4%D7%98%D7%99%D7%A8%D7%95%D7%AA-%D7%A9%D7%A0%D7%94-%D7%97%D7%95%D7%93%D7%A9.xlsx');
 % week = readtable('lmsWeek.xlsx','sheet','2020','Range','B14:L65','ReadVariableNames',false);
-monthAll = readtable('lmsMonth.xlsx','Range','B11:V22','ReadVariableNames',false);
-month70 = readtable('lmsMonth.xlsx','Range','B30:V41','ReadVariableNames',false);
+monthAll = readtable('lmsMonth.xlsx','Range','B11:W22','ReadVariableNames',false);
+month70 = readtable('lmsMonth.xlsx','Range','B30:W41','ReadVariableNames',false);
 %from https://www.cbs.gov.il/he/publications/DocLib/2020/yarhon0720/b1.xls'
 % month70.Var21(8) = sum(week{32:34,end})/21*31;
 % monthAll.Var21(8) = sum(week{32:34,2})/21*31;
-pop = table((2009:2020)',...
-    1000*[7485.6;7623.6;7765.8;7910.5;8059.5;8215.7;8380.1;8546.0;8713.3;8882.8;9054.0;9212.8]);
+pop = table((2009:2021)',...
+    1000*[7485.6;7623.6;7765.8;7910.5;8059.5;8215.7;8380.1;8546.0;8713.3;8882.8;9054.0;9212.8;9500]);
 % correct for medical improvement
-nrm = [ones(11,1),ones(11,1),pop.Var2(2:end)/10^6,pop.Var2(2:end)/10^6];
-b = polyfit((2010:2019)',median(monthAll{:,11:end-1})'./nrm(1:end-1,3),1);
-pred = (2010:2020)'*b(1)+b(2);
-figure;plot((2010:2019)',median(monthAll{:,11:end-1})'./nrm(1:end-1,3));
-hold on;plot(2010:2020,pred(:,1))
-
-b = polyfit((2010:2019)',median(month70{:,11:end-1})'./nrm(1:end-1,3),1);
-pred(:,2) = (2010:2020)'*b(1)+b(2);
-figure;plot((2010:2019)',median(month70{:,11:end-1})'./nrm(1:end-1,3));
-hold on;plot(2010:2020,pred(:,2))
+nrm = [ones(12,1),ones(12,1),pop.Var2(2:end)/10^6,pop.Var2(2:end)/10^6];
+b = polyfit((2010:2019)',median(monthAll{:,11:end-2})'./nrm(1:end-2,3),1);
+pred = (2010:2021)'*b(1)+b(2);
+% figure;plot((2010:2020)',median(monthAll{:,11:end-2})'./nrm(:,3));
+% hold on;plot(2010:2020,pred(:,1))
+% 
+b = polyfit((2010:2019)',median(month70{:,11:end-2})'./nrm(1:end-2,3),1);
+pred(:,2) = (2010:2021)'*b(1)+b(2);
+% figure;plot((2010:2019)',median(month70{:,11:end-1})'./nrm(1:end-1,3));
+% hold on;plot(2010:2020,pred(:,2))
 clear death
 death{1} = monthAll{:,11:end};
 death{2} = month70{:,11:end};
 death{3} = monthAll{:,11:end};
 death{4} = month70{:,11:end};
-lims = [2000 5000;2000 5000;250 550;250 550];
+lims = [2000 6000;2000 6000;250 650;250 650];
 
 yl = {'תמותה','תמותה','תמותה למליון','תמותה למליון'};
 tit = {'תמותה, כל הגילאים','תמותה, מעל 70','תמותה למליון, כל הגילאים','תמותה למליון, מעל 70','עודף תמותה למליון, כל הגילאים','עודף תמותה למליון, מעל 70'};
@@ -35,11 +35,11 @@ figure('units','normalized','position',[0,0,0.65,0.8]);
 for ip = 1:4
     subplot(3,2,ip)
     h = plot(death{ip}./nrm(:,ip)');
-    col = colormap(jet(11));
-    for ii = 1:11
+    col = colormap(jet(13));
+    for ii = 1:12
         h(ii).Color = col(ii,:);
-        if ii == 11
-            h(ii).LineWidth = 2;
+        if ii > 10
+            h(ii).LineWidth = ii-9;
         end
     end
     if ip > 2
@@ -62,7 +62,7 @@ for ip = 1:4
 end
 
 list = readtable('dashboard_timeseries.csv');
-for im = 3:12
+for im = 3:24
     idx = dateshift(list.date,'start','month') == datetime(2020,im,1);
     covid(im,1) = nansum(list.CountDeath(idx))/pop.Var2(end)*10^6;
 end
@@ -74,11 +74,11 @@ for ip = 1:2
     yy = death{ip}./nrm(:,3)'-pred(:,ip)';
     yy = yy - median(yy(:,1:end-1),2);
     h = plot(yy);
-    col = colormap(jet(11));
-    for ii = 1:11
+    col = colormap(jet(13));
+    for ii = 1:12
         h(ii).Color = col(ii,:);
-        if ii == 11
-            h(ii).LineWidth = 2;
+        if ii > 10
+            h(ii).LineWidth = ii-9;
         end
     end
     xlim([1 12])
@@ -95,25 +95,26 @@ for ip = 1:2
     if ip == 1
         hold on
         hc = plot(covid(1:12),'k','linewidth',2);
+        plot(covid(13:end),'k','linewidth',3);
         tot = nansum(yy);
         tot(2,:) = sum(yy(~isnan(yy(:,end)),:));
     end
-    ylim([-50 120])
+    ylim([-50 150])
     set(gca,'YTick',-40:20:120)
 end
 %%
-legend([h;hb;hc],[cellstr(num2str((2010:2020)'));{'צפי';'קורונה'}],[900 415 0.1 0.2]);
+legend([h;hb;hc],[cellstr(num2str((2010:2021)'));{'צפי';'קורונה'}],[900 415 0.1 0.2]);
 set(gcf,'Color','w')
 % niftarim = readtable('~/Downloads/corona_deceased_ver_0034.csv');
-figure;
-hb = bar(tot');
-figure;
-for ii = 1:11
-    hb11(ii) = bar(2009+ii,9.2*tot(2,ii),'facecolor',col(ii,:));
-    hold on
-end
-set(gca,'xtick',2010:2020)
-grid on
-xtickangle(30)
-set(gcf,'Color','w')
-title('תמותה עודפת שנתית')
+% figure;
+% hb = bar(tot');
+% figure;
+% for ii = 1:11
+%     hb11(ii) = bar(2009+ii,9.2*tot(2,ii),'facecolor',col(ii,:));
+%     hold on
+% end
+% set(gca,'xtick',2010:2020)
+% grid on
+% xtickangle(30)
+% set(gcf,'Color','w')
+% title('תמותה עודפת שנתית')
