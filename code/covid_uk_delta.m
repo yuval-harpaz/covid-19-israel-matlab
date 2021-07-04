829b27c8d7d7c9bbfbf31b0392fddf85d3e6ccbf
yuval <yuval@lenovo.2020>
2021-07-04 09:31:29 +0300

    daily update

--- code/covid_uk_delta.m
+++ code/covid_uk_delta.m
@@ -14,12 +14,12 @@ end
 
 cases70 = age_70_74+age_75_79+age_80_84+age_85_89+age_90_;
 
-figure;
-yyaxis left
-plot(hosp.date,hosp.newAdmissions)
-yyaxis right
-plot(date,cases70)
-ylim([0 6000])
+% figure;
+% yyaxis left
+% plot(hosp.date,hosp.newAdmissions)
+% yyaxis right
+% plot(date,cases70)
+% ylim([0 6000])
 %%
 figure;
 yyaxis left
@@ -36,3 +36,24 @@ title('Cases (60+) vs Hospitalizations')
 xtickformat('MMM');
 set(gca,'xtick',datetime(2020,4:30,1))
 xlim([datetime(2020,8,1) datetime('tomorrow')]);
+
+t = table(age_00_04,age_05_09,age_10_14,age_15_19,age_20_24,age_25_29,age_30_34,...
+    age_35_39,age_40_44,age_45_49,age_50_54,age_55_59,age_60_64,age_65_69,age_70_74,age_75_79,age_80_84,age_85_89,age_90_);
+yy = movmean(t{:,:},[3 3]);
+figure;
+h = bar(date,yy,'stacked')
+xlim([datetime(2020,8,1) datetime('tomorrow')])
+
+idx = find(ismember(date,[datetime(2020,10,18),datetime(2020,12,09)]));
+idx(3) = height(t);
+figure;
+h3 = bar(1:3,yy(idx,:),'stacked');
+ag = t.Properties.VariableNames;
+ag = strrep(ag,'age_','');
+ag = strrep(ag,'_','-');
+ag{end} = '90+';
+legend(fliplr(h3),fliplr(ag))
+set(gca,'ygrid','on','XTickLabel',{'18.10.20','09.12.20','28.06.21'})
+xlim([0 5])
+% xlim([datetime(2020,8,1) datetime('tomorrow')])
+% date(idx)
