39d92a28a250fe8256b4442154691345e548512d
yuval <yuval@lenovo.2020>
2021-08-09 16:58:34 +0300

    daily update

--- code/covid_load.m
+++ code/covid_load.m
@@ -38,84 +38,31 @@ end
 if tt.ECMO(end) == 0
     tt = tt(1:find(tt.ECMO > 0,1,'last'),:);
 end
-tt.level1 = tt.mild;
-tt.level2 = (tt.medium+tt.severe-tt.vent)*2;
-tt.level3 = (tt.vent-tt.ECMO_filled)*3;
-tt.level5 = round(tt.ECMO_filled)*5;
-tt.load = tt.level1+tt.level2+tt.level3+tt.level5;
+tt.level1 = tt.mild+tt.medium;
+no_mechanical = tt.severe-tt.vent-tt.ECMO_filled;
+tt.level2 =  no_mechanical*1.5*0.75 + no_mechanical*2*0.25; % no support + oxygen support
+tt.level3 = tt.vent*3;  % (tt.vent-tt.ECMO_filled)*3;
+tt.level6 = round(tt.ECMO_filled)*6;
+tt.load = tt.level1+tt.level2+tt.level3+tt.level6;
 
 writetable(tt,'Load.csv','Delimiter',',','WriteVariableNames',true)
+date = datetime(2021,7,29)+(0:10);
+date = date';
+load = [424,441,500,549,581,646,681,716,700,822,884]';
+
+conf = readtable('confirmed.csv');
+dateSeger = conf.date(ismember(conf.coronaEvents,{'סגר 3','סגר 2','סגר 1 '}));
 %%
 figure;
-hh = plot(tt.date,[tt.level1,tt.level2,tt.level3,tt.level5,tt.load]);
-legend('Mild','Medium + Severe','Vent','ECMO','Load')
-xlim([tt.date(1),datetime('today')])
+hh = plot(tt.date,[tt.level1,tt.level2,tt.level3,tt.level6,tt.load]);
+
+xlim([tt.date(1),datetime('tomorrow')])
 grid on
 title('Load on health system measure   מדד עומס על מערכת הבריאות')
 set(gcf,'Color','w')
-% figure;
-% h1 = bar(tt.date,tt{:,9:end},'stacked','EdgeColor','none');
-% % h1 = bar(tt.date,[tt{:,9:end},listD.CountDeath(70:end)],'stacked','EdgeColor','none');
-% %%
-% figure;
-% yyaxis left
-% h2 = bar(tt.date,tt{:,9:end},'stacked','EdgeColor','none');
-% for ii = 1:4
-%     h2(ii).FaceColor = h1(ii).FaceColor;
-% end
-% ylabel('Load')
-% hold on
-% h2a = bar(tt.date,-listD.CountDeath(70:end),'stacked','FaceColor','k');
-% 
-% yyaxis right
-% 
-% h3 = plot(tt.date,tt.cases,'k');
-% ylabel('Cases')
-% 
-% set(gca,'xtick',datetime(2020,4:30,1),'ygrid','on','FontSize',13)
-% box off
-% grid on
-% 
-% death = listD.CountDeathCum(70:end);
-% for ii = 1:6
-%     kk(ii) = find(death > 1000*ii,1);
-% end
-% hold on
-% h4 = bar(tt.date(kk),repmat(1000,6,1),0.1,'FaceColor',[0.35 0.35 0.35],'EdgeColor','none');
-% ylim([-1000 9000])
-% % legend([h4,h3,h2],{'1k deaths','cases','mild (1)','medium+severe (2)','vent (3)','ECMO (5)'}) 
-% legend([fliplr(h2),h2a,h4,h3],'ECMO (5)','vent (3)','medium+severe (2)','mild (1)','deaths','1k deaths','cases') 
-% set(gcf,'Color','w')
-% 
-% %%
-% figure;
-% yyaxis left
-% h2 = bar(tt.date,tt{:,9:end},1,'stacked','EdgeColor','none');
-% for ii = 1:4
-%     h2(ii).FaceColor = h1(ii).FaceColor;
-% end
-% ylabel('Load')
-% hold on
-% h2a = bar(tt.date,listD.CountDeath(70:end),1,'stacked','FaceColor','k');
-% set(gca, 'YScale', 'log')
-% yyaxis right
-% 
-% h3 = plot(tt.date,tt.cases,'k');
-% ylabel('Cases')
-% 
-% set(gca,'xtick',datetime(2020,4:30,1),'ygrid','on','FontSize',13)
-% box off
-% grid on
-% 
-% death = listD.CountDeathCum(70:end);
-% for ii = 1:6
-%     kk(ii) = find(death > 1000*ii,1);
-% end
-% hold on
-% h4 = bar(tt.date(kk),repmat(1000,6,1),0.1,'FaceColor',[0.35 0.35 0.35],'EdgeColor','none');
-% % ylim([-1000 9000])
-% % legend([h4,h3,h2],{'1k deaths','cases','mild (1)','medium+severe (2)','vent (3)','ECMO (5)'}) 
-% legend([fliplr(h2),h2a,h4,h3],'ECMO (5)','vent (3)','medium+severe (2)','mild (1)','deaths','1k deaths','cases') 
-% set(gcf,'Color','w')
-% 
-% set(gca, 'YScale', 'log')
+hold on
+plot(date,load,'.k','MarkerSize',20)
+idx = ismember(tt.date,dateSeger(2:end));
+plot(tt.date(idx),tt.load(idx),'*b','MarkerSize',5)
+legend('Mild+Medium    קל+בינוני','Severe    קשה','Vent     מונשים','ECMO    אקמו',...
+    'Load       עומס','official load value    מדד העומס הרשמי','lockdown  סגר')
