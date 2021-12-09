90b0c1de88fbbe2ca821f8625739282fa1bdbd7e
yuval <yuval@lenovo.2020>
2021-12-09 23:31:31 +0200

    SA csv shortened

--- code/covid_SA.py
+++ code/covid_SA.py
@@ -1,17 +1,7 @@
 import pandas as pd
 import numpy as np
-import plotly.graph_objects as go
-import dash
-from dash import dcc
-from dash import html
-import plotly.express as px
-import os
-from dash.dependencies import Input, Output
-import dash_bootstrap_components as dbc
 import urllib.request
 import json
-
-
 
 url1 = 'https://raw.githubusercontent.com/alex1770/Covid-19/master/VOCgrowth/EarlyOmicronEstimate/extracthospdata/SouthAfricaHospData.json'
 with urllib.request.urlopen(url1) as api1:
@@ -19,16 +9,6 @@ with urllib.request.urlopen(url1) as api1:
 date = list(data1.keys())
 region = list(data1[date[0]].keys())
 field = list(data1[date[0]][region[0]].keys())
-# Date = []
-# Facilities_Reporting = []
-# Admissions_to_Date = []
-# Died_to_Date = []
-# Discharged_to_Date = []
-# Currently_Admitted = []
-# Currently_in_ICU = []
-# Currently_Ventilated = []
-# Currently_Oxygenated = []
-# Admissions_in_Previous_Day = []
 column = [[],[],[],[],[],[],[],[],[],[],[]]
 for dd in date:
     for rr in region:
