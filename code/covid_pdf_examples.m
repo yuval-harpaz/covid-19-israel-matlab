500cf9a3e7fec213d5190f2dffed675ad668b48c
yuval <yuval@lenovo.2020>
2022-01-21 23:22:17 +0200

    update

--- code/covid_pdf_examples.m
+++ code/covid_pdf_examples.m
@@ -65,4 +65,48 @@ title('Current method for computing R')
 
 
 
-
+%%
+listD = readtable('~/covid-19-israel-matlab/data/Israel/dashboard_timeseries.csv');
+R = covid_R31(listD.tests_positive1);
+% mu = 2.5:0.5:4.5;
+% sd = mu/4.5*3.5;
+for ii = [12,17]
+    R(:,end+1) = covid_R31(listD{:,ii});
+end
+R2_5 = covid_R31(listD.tests_positive1,[2.5,2.5*3.5/4.5]);
+% mu = 2.5:0.5:4.5;
+% sd = mu/4.5*3.5;
+for ii = [12,17]
+    R2_5(:,end+1) = covid_R31(listD{:,ii},[2.5,2.5*3.5/4.5]);
+end
+ccc = [0,0.4470,0.7410;0.8500,0.3250,0.09800;0.9290,0.6940,0.1250;0.4940,0.1840,0.5560;0.4660,0.6740,0.1880;0.3010,0.7450,0.9330;0.6350,0.07800,0.1840];
+casesA = movmean(listD.tests_positive1,[3 3]);
+casesA(isnan(casesA)) = 0;
+casesA = casesA/max(casesA)*2+0.5;
+%%
+figure;
+fill([listD.date;flipud(listD.date)],[casesA;0.5+zeros(size(casesA))],[0.8 0.8 0.8], 'linestyle','none')
+hold on
+hh3 = plot(listD.date(19:end-11), R(26:end-4,:), 'linewidth',1.5);
+hh3(1).Color = [0.3 0.7 0.3];
+hh3(2).Color = ccc(4,:);
+hh3(3).Color = ccc(3,:);
+legend(hh3, 'cases','new hospitalizations','new severe','location','northwest')
+grid on
+box off
+title('R')
+ylabel('R')
+set(gca,'XTick',datetime(2020,1:300,1))
+xtickangle(90)
+xtickformat('MM/yy')
+%%
+figure;
+hh3 = plot(listD.date(19:end-11), R2_5(26:end-4,:), 'linewidth',1.5);
+hh3(1).Color = [0.3 0.7 0.3];
+hh3(2).Color = ccc(4,:);
+hh3(3).Color = ccc(3,:);
+legend('cases','new hospitalizations','new severe')
+grid on
+box off
+title('R')
+ylabel('R')
