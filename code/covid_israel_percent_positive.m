ea46d01a6ffc32cefdf4d14b2ec482c46da2be5a
yuval <yuval@lenovo.2020>
2022-01-30 14:09:37 +0200

    update

--- code/covid_israel_percent_positive.m
+++ code/covid_israel_percent_positive.m
@@ -65,15 +65,14 @@ set(gca,'xtick',datetime(2020,3:30,1))
 ccc = [0,0.4470,0.7410;0.8500,0.3250,0.09800;0.9290,0.6940,0.1250;0.4940,0.1840,0.5560;0.4660,0.6740,0.1880;0.3010,0.7450,0.9330;0.6350,0.07800,0.1840];
 fig1 = figure('position',[50,50,800,500]); %#ok<NASGU>
 hold on
-
-hh(2) = plot(dateSmooth(1:end-3),posSmooth(1:end-3),'linewidth',2,'Color',ccc(4,:));
-hh(1) = plot(dateSmooth(1:end-3),posPCR(1:end-3),'linewidth',2,'Color',ccc(1,:));
-hh(3) = plot(dateSmooth(1:end-3),posAnti(1:end-3),'linewidth',2,'Color',ccc(2,:));
-hh(4) = plot(dateSmooth(end-2:end),posPCR(end-2:end),'.');
-hh(5) = plot(dateSmooth(end-2:end),pos(end-2:end),'.');
-hh(6) = plot(dateSmooth(end-2:end),posAnti(end-2:end),'.');
+hhp(2) = plot(dateSmooth(1:end-3),posSmooth(1:end-3),'linewidth',2,'Color',ccc(4,:));
+hhp(1) = plot(dateSmooth(1:end-3),posPCR(1:end-3),'linewidth',2,'Color',ccc(1,:));
+hhp(3) = plot(dateSmooth(1:end-3),posAnti(1:end-3),'linewidth',2,'Color',ccc(2,:));
+hhp(4) = plot(dateSmooth(end-2:end),posPCR(end-2:end),'.');
+hhp(5) = plot(dateSmooth(end-2:end),pos(end-2:end),'.');
+hhp(6) = plot(dateSmooth(end-2:end),posAnti(end-2:end),'.');
 for ii = 1:3
-    hh(ii+3).Color = hh(ii).Color;
+    hhp(ii+3).Color = hhp(ii).Color;
 end
 ylabel(['positive ','(%)',' בדיקות חיוביות '])
 set(gca,'ygrid', 'on','fontsize',13)
@@ -83,37 +82,12 @@ set(gcf,'Color','w')
 grid minor
 box off
 title({'אחוז הבדיקות החיוביות','% positive tests'})
-legend(hh,'PCR','All','Antigen','location','northwest')
+legend(hhp(1:3),'PCR','All','Antigen','location','northwest')
 xtickformat('MMM')
 set(gca,'xtick',datetime(2020,3:30,1))
 new = copyobj(gca,gcf);
-set(new,'YAxisLocation','right');
-%%
-% 
-% fig1 = figure('position',[50,50,800,500]); %#ok<NASGU>
-% yyaxis left
-% plot(list.date(idx),list.new_hospitalized(idx),'.');
-% hold on
-% hh(1) = plot(list.date(idx),hospSmooth,'linewidth',2,'linestyle','-');
-% ylabel(['new hospitalized     ','מאושפזים חדשים'])
-% ax = gca;
-% ax.YRuler.Exponent = 0;
-% yyaxis right
-% plot(dateSmooth,pos,'.')
-% hold on
-% hh(2) = plot(dateSmooth,posSmooth,'linewidth',2,'linestyle','-');
-% ylabel(['positive ','(%)',' בדיקות חיוביות '])
-% set(gca,'ygrid', 'on','fontsize',13)
-% % xlim([list.date(idx(1))-1 list.date(idx(end))+1]);
-% set(gcf,'Color','w')
-% ylim([0 15])
-% grid minor
-% box off
-% title({'אחוז הבדיקות החיוביות ומספר המאושפזים החדשים','% positive tests vs new hospitalized'})
-% legend(hh,'hospitalized מאושפזים','positive בדיקות חיוביות','location','northwest')
-% xtickformat('MMM')
-% set(gca,'xtick',datetime(2020,3:30,1))
-
+set(new,'YAxisLocation','right','Color','none');
+% legend(hhp(1:3),'PCR','All','Antigen','location','northwest')
 
 function sm = smot(vec)
 sm = movmean(vec,[3 3]);
