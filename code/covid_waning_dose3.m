dfd9e1cadb79db03f1727c4534932fd2f05d6a8a
yuval <yuval@lenovo.2020>
2022-03-12 20:10:26 +0200

    update

--- code/covid_waning_dose3.m
+++ code/covid_waning_dose3.m
@@ -49,47 +49,49 @@ yy1 = yy1(:,2:end,:);
 xtick1 = xtick1-1;
 yy(end+1:size(yy1,1),end+1:size(yy1,2),:) = nan;
 %%
-xEnd = length(weekInfec)+0.5;
-figure;
-subplot(2,1,1)
-imagesc(yy(:,:,2))
-set(gca,'Ydir','normal')
-set(gca,'Xtick',[xtick; xEnd],'XtickLabel',[xtl;datestr(weekInfec(end)+6,'dd/mm')],...
-    'Ytick',[2;xtick(2:end)],'YtickLabel',xtl)
-colormap('hot')
-ylim([1.5 12.5])
-caxis([0 3000])
-% xlim([0.5 length(yy)+0.5])
+for clim = [3000, 30000];
+    xEnd = length(weekInfec)+0.5;
+    figure;
+    subplot(2,1,1)
+    imagesc(yy(:,:,2))
+    set(gca,'Ydir','normal')
+    set(gca,'Xtick',[xtick; xEnd],'XtickLabel',[xtl;datestr(weekInfec(end)+6,'dd/mm')],...
+        'Ytick',[2;xtick(2:end)],'YtickLabel',xtl)
+    colormap('hot')
+    ylim([1.5 12.5])
+    caxis([0 clim])
+
+    % xlim([0.5 length(yy)+0.5])
 
 
-hold on
-hh(1) = fill([0,0,0,0],[0,0,0,0],[1 1 1]);
-hh(2) = line([xEnd,xEnd],[0,12.5],'Color','b');
-yl3 = yy(2,:,2)/3000*12.5+2;
-yl3(yl3 == 2) = nan;
-yl3(1) = nan;
-hh(3) = plot(yl3,'g');
-lg = legend(hh,'3000 cases per M','last update','first group to vaccinate');
-set(lg, 'Color','none','TextColor','w','Box','off')
-title('Cases per million by Infection time and by dose III vaccination group')
-xlabel('Infection week')
-ylabel('Dose III vaccination week')
-subplot(2,1,2)
-imagesc(yy1(:,:,2))
-set(gca,'Ydir','normal')
-set(gca,'Xtick',xtick1,'XtickLabel',xtl1,'Ytick',xtick1,'YtickLabel',xtl1)
-colormap('hot')
-ylim([1.5 12.5])
-caxis([0 3000])
-set(gcf,'Color','w')
-hold on
-yl1 = yy1(2,:,2)/3000*12.5+2;
-yl1(1) = nan;
-plot(yl1,'g')
-title('Cases per million by Infection time and by dose I vaccination group')
-xlabel('Infection week')
-ylabel('Dose I vaccination week')
-
+    hold on
+    hh(1) = fill([0,0,0,0],[0,0,0,0],[1 1 1]);
+    hh(2) = line([xEnd,xEnd],[0,12.5],'Color','b');
+    yl3 = yy(2,:,2)/clim*12.5+2;
+    yl3(yl3 == 2) = nan;
+    yl3(1) = nan;
+    hh(3) = plot(yl3,'g');
+    lg = legend(hh,[str(clim),' cases per M'],'last update','first group to vaccinate');
+    set(lg, 'Color','none','TextColor','w','Box','off')
+    title('Cases per million by Infection time and by dose III vaccination group')
+    xlabel('Infection week')
+    ylabel('Dose III vaccination week')
+    subplot(2,1,2)
+    imagesc(yy1(:,:,2))
+    set(gca,'Ydir','normal')
+    set(gca,'Xtick',xtick1,'XtickLabel',xtl1,'Ytick',xtick1,'YtickLabel',xtl1)
+    colormap('hot')
+    ylim([1.5 12.5])
+    caxis([0 clim])
+    set(gcf,'Color','w')
+    hold on
+    yl1 = yy1(2,:,2)/clim*12.5+2;
+    yl1(1) = nan;
+    plot(yl1,'g')
+    title('Cases per million by Infection time and by dose I vaccination group')
+    xlabel('Infection week')
+    ylabel('Dose I vaccination week')
+end
 %%
 co = flipud(jet(10));
 co(end+1:100,3) = 2/3;
@@ -188,14 +190,15 @@ ylabel('vaccination week')
 zlabel('cases per million')
 title('Infections by time from dose III, 60+')
 grid on
+views = [-15;-15;-15;(-15:-15:-175)';-175;-175;-175]+3;
+views(:,2) = 43;
 view(views(vv,:))
 zlim([0 40000])
 set(gca,'ZTick',0:10000:40000)
 % axis square
 set(gcf,'Color','w')
 %%
-views = [-15;-15;-15;(-15:-15:-175)';-175;-175;-175]+3;
-views(:,2) = 43;
+
 fig25 = figure('units','normalized','position',[0.1 0 0.7 1]);
 for vv = 1:length(views)
     subplot(2,1,1)
