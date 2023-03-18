b2f5fc9b292455f2a38409447453d70840b80b2c
yuval <yuval@lenovo.2020>
2023-03-18 11:46:40 +0200

    check prev better

--- code/covid_logscale.py
+++ code/covid_logscale.py
@@ -9,11 +9,24 @@ import plotly.graph_objects as go
 import urllib.request
 import json
 import requests
-
+api = 'https://datadashboardapi.health.gov.il/api/queries/'
 local = '/home/innereye/covid-19-israel-matlab/'
 if os.path.isdir(local):
     os.chdir(local)
-def movmean(vec, win, nanTail=False):
+    remote = False
+else:
+    remote = True
+
+update = requests.get(api+'lastUpdate', verify=False).json()
+update = update[0]['lastUpdate'].replace('T', ' ')[:-5]
+with open('docs/hospitalizations.html', 'r') as file:
+    prev_update = file.read()[15:34]
+
+if update == prev_update:
+    print('no news')
+    if remote:
+        sys.exit(0)
+def movmean(vec, win, nanTail=False, round=0):
     #  smooth a vector with a moving average. win should be an odd number of samples.
     #  vec is np.ndarray size (N,) or (N,0)
     #  to get smoothing of 3 samples back and 3 samples forward use win=7
@@ -24,6 +37,7 @@ def movmean(vec, win, nanTail=False):
             smooth[:] = np.nan
         for ii in range(int(win/2), len(vec)-int(win/2)):
             smooth[ii] = np.nanmean(vec[ii-int(win/2):ii+int(win/2)+1])
+    smooth = np.round(smooth, round)
     return smooth
 
 # if os.path.isfile('/home/innereye/Downloads/VerfiiedVaccinationStatusDaily'):
@@ -31,8 +45,7 @@ def movmean(vec, win, nanTail=False):
 #     dfAge = pd.read_csv(
 #         '/home/innereye/covid-19-israel-matlab/data/Israel/cases_by_age.csv')
 # else:
-api = 'https://datadashboardapi.health.gov.il/api/queries/'
-warning = ''
+
 # try:
 #     dfAge = pd.read_csv('https://raw.githubusercontent.com/yuval-harpaz/covid-19-israel-matlab/master/data/Israel/cases_by_age.csv')
 #     dfAge.to_csv('cases_by_age.csv', index=False, sep=',')
@@ -42,8 +55,8 @@ warning = ''
 try:
     dfTS = pd.read_json(requests.get(api+'hospitalizationStatus', verify=False).text)
     prev = pd.read_csv('data/Israel/hospitalizationStatus.csv')
-    if prev.loc[len(prev)-1, 'date'] == dfTS.loc[len(dfTS)-1, 'dayDate'][:10]:
-        raise Exception('no new dates')
+    # if prev.loc[len(prev)-1, 'date'] == dfTS.loc[len(dfTS)-1, 'dayDate'][:10]:
+    #     raise Exception('no new dates')
     cn = list(dfTS.columns)
     cn[0] = 'date'
     dfTS.columns = cn
@@ -67,24 +80,27 @@ layout = go.Layout(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)', 
 layout1 = go.Layout(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)')
 columns = ['newHospitalized', 'newSevere','vent','countDeath']
 label = ['מאושפזים', 'חולים-קשה' , 'מונשמים','נפטרים']
+round = [0, 0, 1, 1]
 fig1 = go.Figure(layout=layout1)
 for ii in range(len(columns)):
-    fig1.add_trace(go.Scatter(x=dfTS['date'], y=movmean(dfTS[columns[ii]].to_numpy(), 7, True),
+    fig1.add_trace(go.Scatter(x=dfTS['date'], y=movmean(dfTS[columns[ii]].to_numpy(), 7, True, round[ii]),
                               mode='lines', line_color='#%02x%02x%02x' % cco[ii+2], name=label[ii]))
 fig1.update_yaxes(showgrid=True, gridwidth=1, gridcolor='lightgray', type='log', side='right')
-fig1.update_yaxes(range=(-0.25, 3))
-fig1.update_xaxes(showgrid=True, gridwidth=1, gridcolor='lightgray', dtick="M1", tickformat="%b\n%Y")
+# fig1.update_yaxes(range=(-0.25, 3), minor_ticks=dict(ticks="inside", ticklen=6, showgrid=True))
+fig1.update_yaxes(range=(-0.25, 3), tickmode='array', tickvals=[1,10,100,1000])
+fig1.update_xaxes(showgrid=True, gridwidth=1, gridcolor='lightgray', dtick="M1", tickformat="%d/%m\n%Y")
 fig1.update_layout(legend=dict(yanchor="top", y=0.99, xanchor="left", x=0.85, font=dict(size=20)))
 fig1.update_layout(title=dict(text="קורונה בישראל, מקרים חדשים לפי חומרה", font_size=40, xanchor='center', x=0.5), font_size=20)
 # fig1.show()
 
 
-txt0 = '<h1>Israel COVID19 data from the Ministry of Health</h1>'+\
-    '<h2>New cases by condition</h2>'
-txt = txt0+'by <a href="https://twitter.com/yuvharpaz" target="_blank">@yuvharpaz</a>. '+\
-      ' <a href='+api+'hospitalizationStatus>source</a>, '+\
-      '<a href="https://github.com/yuval-harpaz/covid-19-israel-matlab/blob/master/data/Israel/hospitalizationStatus.csv">sheet</a><br>'
-txt = txt+'see also <a href="https://yuval-harpaz.github.io/covid-19-israel-matlab/by_vacc_abs.html" target="_blank">Cases by Vaccination Status</a><br>'
+txt0 = '<!last update: '+update+'>\n' #+ \
+    #   '<h1>תחלואת קורונה בישראל, מנתוני משרד הבריאות</h1>'
+    # '<h2>New cases by condition</h2>'
+txt = txt0+'by <a href="https://twitter.com/yuvharpaz" target="_blank">@yuvharpaz</a>. ' + \
+      ' <a href='+api+'hospitalizationStatus>source</a>, ' + \
+      '<a href="https://github.com/yuval-harpaz/covid-19-israel-matlab/blob/master/data/Israel/hospitalizationStatus.csv">sheet</a>'
+txt = txt+'. see also <a href="https://yuval-harpaz.github.io/covid-19-israel-matlab/by_vacc_abs.html" target="_blank">Cases by Vaccination Status</a><br>'
 txt = txt+fig1.to_html()
 file = open("docs/hospitalizations.html", "w")
 a = file.write(txt)
